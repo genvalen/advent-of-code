a5321e579c2d7a86f72d71c71d7a7fa17d0adabd
genvalen <genvalen@protonmail.com>
2020-02-05 16:32:32 -0500

    Edit solution format

--- day_1/solution-part-2.py
+++ day_1/solution-part-2.py
@@ -1,4 +1,5 @@
 # The Tyranny of the Rocket Equations Pt. 2
+# https://adventofcode.com/2019/day/1
 def fuel_calculator(mass):
     f = int(mass)//3-2
     if f > 0:
@@ -9,4 +10,4 @@ def fuel_calculator(mass):
 modules = open('day_1/data.txt', 'r')
 fuel_per_mod = [fuel_calculator(m) for m in modules]
 modules.close()
-print(sum(fuel_per_mod))
+print("Sum of fuel requirements:", sum(fuel_per_mod))
