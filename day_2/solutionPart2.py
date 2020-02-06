53a0b28c00477a59fd8fc9b57d73782411a1ebd6
genvalen <genvalen@protonmail.com>
2020-02-06 00:51:48 -0500

    Edit comments and slightly modify code

--- day_2/solutionPart2.py
+++ day_2/solutionPart2.py
@@ -3,9 +3,10 @@
 from solutionPart1 import opcode1, opcode2
 from itertools import product
 
-# Code terminology updated according to problem specifications
+# Simmilar script to part 1 main, but as a function;
+# Function terminology changed according to AoC specifications
 def noun_verb_locator(src):
-    '''Transform memory based on opcodes and return position zero.'''
+    """Transform memory based on opcodes/instructions and return position zero."""
     memory = src
     instruction_pointer = 4
     for i in range(0, len(memory), instruction_pointer):
@@ -18,21 +19,27 @@ def noun_verb_locator(src):
             opcode1(memory, params)
         elif instruction == 2:
             opcode2(memory, params)
+
     return(memory[0])
     
+
 if __name__ == '__main__':
     desired_output = 19690720
     noun_verb_pairs = list(product(list(range(0,100)), repeat=2))
 
-    # Try noun/verb combinations against desired output
+    # Reset computer memory for each noun/verb combination
+    # Try each combination output against desired output
     for p in noun_verb_pairs:
-        source = open('day_2/data.txt', 'r')
-        memory = [list(map(int, i.split(','))) for i in source][0]
-        source.close()
+        with open("day_2/data.txt", "r") as source:
+            memory = [list(map(int, i.split(','))) for i in source][0]
+
+        # Update combinations
         memory[1] = p[0]
         memory[2] = p[1]
 
-        # Solution
+        # If output matches desired output
+        # print out solution (computed equation) and break
         if noun_verb_locator(memory) == desired_output:
-            print(100*p[0]+p[1])
+            print("Solution:", 100*p[0]+p[1])
+            break
 
