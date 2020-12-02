4bb2311645e1ea4ed16afdcb2a7f8696d314abd3
genvalen <genvalen@protonmail.com>
2020-12-02 04:04:01 -0500

    Update root folder

---
+++ advent-of-code-2020/day-1/solution.py
@@ -0,0 +1,20 @@
+def part_one(nums):
+        for i in nums:
+            for j in nums[1:]:
+                if i + j == 2020:
+                    return i * j
+
+
+def part_two(nums):
+        for i in nums:
+            for j in nums[1:]:
+                for k in nums[2:]:
+                    if i + j + k == 2020:
+                        return i * j * k
+
+
+with open("day-1/data.txt") as f:
+        data = [int(line.strip()) for line in f]
+
+print("Part one:", part_one(data))
+print("Part two:", part_two(data))
