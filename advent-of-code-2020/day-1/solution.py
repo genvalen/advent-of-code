ec1777f59b07603d05928dca7973a9915455c412
genvalen <genvalen@protonmail.com>
2021-02-07 16:33:48 -0500

    update part one to use two sum algorithm

--- advent-of-code-2020/day-1/solution.py
+++ advent-of-code-2020/day-1/solution.py
@@ -1,20 +1,31 @@
 def part_one(nums):
-        for i in nums:
-            for j in nums[1:]:
-                if i + j == 2020:
-                    return i * j
+    seen = set()
+    for num in nums:
+        if not seen:
+            seen.add(num)
+
+        target = abs(2020 - num)
+
+        if target in seen:
+            return num * target
+        
+        seen.add(num)
+        
+    return -1
+
 
 
 def part_two(nums):
-        for i in nums:
-            for j in nums[1:]:
-                for k in nums[2:]:
-                    if i + j + k == 2020:
-                        return i * j * k
+    for i in nums:
+        for j in nums[1:]:
+            for k in nums[2:]:
+                if i + j + k == 2020:
+                    return i * j * k
 
+if __name__ == "__main__":
+    with open("data.txt") as f:
+            data = [int(line.strip()) for line in f]
 
-with open("advent-of-code-2020/day-1/data.txt") as f:
-        data = [int(line.strip()) for line in f]
-
-print("Part one:", part_one(data))
-print("Part two:", part_two(data))
+    print("Part one:", part_one(data))
+    print("Part two:", part_two(data))
+    
