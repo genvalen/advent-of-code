41457b9a43a299eae88d47947bac39b6bc0f6310
genvalen <genvalen@protonmail.com>
2021-08-16 19:18:36 -0400

    Update day 07 part 1, add part 2

--- advent-of-code-2020/day-7/solution.py
+++ advent-of-code-2020/day-7/solution.py
@@ -1,11 +1,13 @@
-def part_one(data):
+# Handy Haversacks
+# https://adventofcode.com/2020/day/7
+
+def part_one(data: list, target: str = "shiny gold bag") -> int:
+    """Immplements topological sort.
+
+    Returns the number of unique bags that are able to hold
+    (directly or indirectly) the target bag.
     """
-    Use input data to mimic adjaceny list;
-    apply topological sorting to filter through adj-list(dict);
-    Return num of bags which directly/indirectly hold target.
-    """
-    bags = dict()
-    target = "shiny gold bag"
+    bag_types = dict()
     contains_target = set()
     count = 0
 
@@ -13,45 +15,67 @@ def part_one(data):
         line = line.strip()
         key, value = line.split(" contain ")
 
-        # make key (bag-color) singular, !plural
+        # remove 's' from '...bags' to make singular, not plural
+        # for consistency when traversing outer layers in the sort
         key = key[:-1]
 
-        # identify bags that directly contain target
-        # add all other bags into dict
         if target in value:
             contains_target.add(key)
             count += 1
             continue
 
-        bags[key] = value
+        bag_types[key] = value
 
     while contains_target:
         bag = contains_target.pop()
 
-        visited = set()
-        for k, v in bags.items():
+        seen = set()
+        for k, v in bag_types.items():
             if bag in v:
                 count += 1
-                visited.add(k)
+                seen.add(k)
                 contains_target.add(k)
 
-        #remove visited bags from dict
-        for bag in visited:
-            del bags[bag]
+        #remove bags from dict
+        for bag in seen:
+            del bag_types[bag]
 
     return count
 
 
-def part_two(data):
-    pass
+def part_two(data, target: str = "shiny gold bag") -> int:
+    """Returns the number of unique bags that the target bag contains."""
+    bags_to_search = dict()
+
+    for line in data:
+        line = line.strip()
+        key, value = line.split(" contain ")
+
+        if target in key:
+            for bag in value.split(", "):
+                if "no other bags." in bag:
+                    break
+
+                quantity, color = bag.split(" ", 1)
+                color = color.strip(".")
+                bags_to_search[color] = int(quantity)
+
+            break
+
+    # adjust total so that outermost bag (intitial target)
+    # is not included in the final count. Otherwise,
+    # counts reflect number of bags contained within a bag, plus itself.
+    total = 0 if target == "shiny gold bag" else 1
+
+    for color, quantity in bags_to_search.items():
+        total += quantity * part_two(data, color)
+
+    return total
 
 
 if __name__ == "__main__":
-    with open("sample2.txt") as f:
+    with open("data.txt") as f:
             data = f.readlines()
-    # with open("output.txt", "a") as f:
-    #     f.write(str(part_one(data)) + "\n")
 
     print("Part one:", part_one(data))
-    # print("Part two:", part_two(data))
-    
+    print("Part two:", part_two(data))
