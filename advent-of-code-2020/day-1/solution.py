1732fbef3b5b07a14508407b2c6320a9e5111aac
genvalen <genvalen@protonmail.com>
2021-08-16 05:24:52 -0400

    Update day 01 part 2

--- advent-of-code-2020/day-1/solution.py
+++ advent-of-code-2020/day-1/solution.py
@@ -1,31 +1,41 @@
-def part_one(nums):
+def part_one(nums: list, total: int = 2020) -> int:
+    nums.sort()
     seen = set()
+
     for num in nums:
         if not seen:
             seen.add(num)
+            continue
 
-        target = abs(2020 - num)
+        target = total - num
 
         if target in seen:
             return num * target
-        
+
         seen.add(num)
-        
-    return -1
+
+    return
 
 
+def part_two(nums: list, total: int = 2020) -> int:
+    nums.sort()
 
-def part_two(nums):
-    for i in nums:
-        for j in nums[1:]:
-            for k in nums[2:]:
-                if i + j + k == 2020:
-                    return i * j * k
+    for num in nums:
+        two_sum_target = total - num
+        sub_list = nums.copy()
+        sub_list.remove(num)
+        two_sum_result = part_one(sub_list, two_sum_target)
+
+        if two_sum_result:
+            return num * two_sum_result
+
+    return
+
 
 if __name__ == "__main__":
     with open("data.txt") as f:
-            data = [int(line.strip()) for line in f]
+        data = [int(line.strip()) for line in f]
 
     print("Part one:", part_one(data))
     print("Part two:", part_two(data))
-    
+ 
