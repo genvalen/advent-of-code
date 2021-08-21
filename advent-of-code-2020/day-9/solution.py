87df361dc103edb3015046431eb2c0ff897e6693
genvalen <genvalen@protonmail.com>
2021-08-21 04:43:05 -0400

    Add day 09 part 2

--- advent-of-code-2020/day-9/solution.py
+++ advent-of-code-2020/day-9/solution.py
@@ -6,7 +6,7 @@ from collections import deque
 
 def part_one(data: List[int]) -> int:
     """Returns the first element in `data` which does not have a pair of numbers
-    among the previous 25 elements before it that add up to it.
+    among the previous 25 elements before it that add up to itself.
     """
     options = deque(data[:25], maxlen=25)
 
@@ -18,7 +18,7 @@ def part_one(data: List[int]) -> int:
 
         options.append(candidate)
 
-    return
+    return -1
 
 
 def has_two_sum(data: List[int], total: int) -> bool:
@@ -38,8 +38,35 @@ def has_two_sum(data: List[int], total: int) -> bool:
     return False
 
 
-def part_two(data):
-    pass
+def part_two(data: List[int]) -> int:
+    """Find a contiguous set of numbers that add up to a target and return
+    the sum of the smallest and largest number in the set."""
+    target = part_one(data)
+    length = len(data)
+    ptr1, ptr2 = 0, 1
+
+    while ptr2 < length:
+        cur_sum = sum(data[ptr1:ptr2+1])
+
+        if cur_sum == target:
+            sublist = data[ptr1:ptr2+1]
+            return min(sublist) + max(sublist)
+
+        # increase sublist
+        ptr2 += 1
+
+        # decrease sublist from the left, if needed
+        if cur_sum > target:
+            while cur_sum > target and ptr2 < length:
+                ptr1 += 1
+
+                # Ensure sublist maintains a min of two elements
+                if ptr2 - ptr1 == 0:
+                    ptr2 += 1
+
+                cur_sum = sum(data[ptr1:ptr2+1])
+    
+    return -1
 
 
 if __name__ == "__main__":
