9ad135770ad3e0904843ea5aacee93cf3b4e1a27
genvalen <genvalen@protonmail.com>
2022-01-11 23:48:06 -0500

    Add day 06

---
+++ advent-of-code-2021/day-06/solution.py
@@ -0,0 +1,20 @@
+# Lanternfish
+# https://adventofcode.com/2021/day/6
+
+from typing import List
+
+
+def part_one(data: List[int], day=18):
+    pass
+
+
+def part_two(data):
+    pass
+
+
+if __name__ == "__main__":
+    with open("data.txt") as f:
+        data = list(map(int, f.read().split(",")))
+
+    print("Part one:", part_one(data))
+    print("Part two:", part_two(data))
